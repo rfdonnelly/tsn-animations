1eae7d0b794c55855e80eaf531e5da8bc15c3fd1
Rob Donnelly <rfdonnelly@gmail.com>
2024-10-18 22:13:41 -0700

    refactor

--- 802.1AS/peer_link_delay_measurement.py
+++ 802.1AS/peer_link_delay_measurement.py
@@ -1,5 +1,43 @@
 from manim import *
 
+class LabeledArrow(Arrow):
+    def __init__(
+        self,
+        label: str,
+        color: ParsableManimColor = WHITE,
+        *args,
+        **kwargs,
+    ) -> None:
+        super().__init__(*args, color=color, **kwargs)
+
+        def angle():
+            print("%s %f" % (label, self.get_angle()))
+            if self.get_angle() < -PI/2:
+                return self.get_angle() - PI
+            else:
+                return self.get_angle()
+
+        self.add(
+            Text(label, color=color)
+                .rotate(angle())
+                .move_to(self)
+                .shift(UP * MED_SMALL_BUFF)
+        )
+
+class Envelope(Rectangle):
+    def __init__(
+        self,
+        color: ParsableManimColor = WHITE,
+        *args,
+        **kwargs,
+    ) -> None:
+        size = MED_SMALL_BUFF
+
+        super().__init__(*args, height=size, width=size*1.5, color=color, **kwargs)
+
+        self.add(Line(start=self.get_corner(UL), end=self.get_center(), color=color))
+        self.add(Line(start=self.get_center(), end=self.get_corner(UR), color=color))
+
 class PeerLinkDelayMeasurement(Scene):
     def construct(self):
         Text.set_default(font_size = 12)
@@ -30,50 +68,28 @@ class PeerLinkDelayMeasurement(Scene):
         tr_line = seqline(tr.get_bottom() + DOWN * 0.5, color=BLUE)
         self.play(Create(tt_line), Create(tr_line))
 
-        def message(start, end, color=WHITE):
-            msg_size = 0.3
-            msg = Rectangle(height = msg_size, width = msg_size * 1.5, color=color)
-            (
-                msg
-                    .add(Line(start=msg.get_corner(UL), end=msg.get_center(), color=color))
-                    .add(Line(start=msg.get_center(), end=msg.get_corner(UR), color=color))
-                    .move_to(start).shift(UP * 0.5)
-            )
-            return {
-                'create': GrowFromCenter(msg),
-                'transmit': msg.animate.move_to(end).shift(UP * 0.5),
-                'consume': ShrinkToCenter(msg),
-            }
-
         def tr_to_tt(text, start, end, color=WHITE):
-            line = Arrow(start=start, end=end, color=color)
-            line.add(
-                Text(text, color=color)
-                    .rotate(line.get_angle() - PI)
-                    .move_to(line)
-                    .shift(UP * 0.3)
-            )
-            msg = message(tr, tt, color=color)
+            arrow = LabeledArrow(text, start=start, end=end, color=color)
+            msg = Envelope(color=color).move_to(tr).shift(UP * MED_LARGE_BUFF)
             self.play(Succession(
-                msg['create'],
+                GrowFromCenter(msg),
                 AnimationGroup(
-                    GrowArrow(line),
-                    msg['transmit']
+                    GrowArrow(arrow),
+                    msg.animate.move_to(tt).shift(UP * MED_LARGE_BUFF)
                 ),
-                msg['consume']
+                ShrinkToCenter(msg),
             ))
 
         def tt_to_tr(text, start, end, color=WHITE):
-            # line = Arrow(start=start, end=end, color=color)
-            line = LabeledArrow(Text(text, color=color), start=start, end=end, color=color)
-            msg = message(tt, tr, color=color)
+            arrow = LabeledArrow(text, start=start, end=end, color=color)
+            msg = Envelope(color=color).move_to(tt).shift(UP * MED_LARGE_BUFF)
             self.play(Succession(
-                msg['create'],
+                GrowFromCenter(msg),
                 AnimationGroup(
-                    GrowArrow(line),
-                    msg['transmit']
+                    GrowArrow(arrow),
+                    msg.animate.move_to(tr).shift(UP * MED_LARGE_BUFF)
                 ),
-                msg['consume']
+                ShrinkToCenter(msg),
             ))
 
         tr_pos = tr_line.get_start() + DOWN * 0.2
